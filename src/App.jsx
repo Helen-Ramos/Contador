d11966d03e588c844f0256e819f687d5d0048bf2
Helen-Ramos <helensramos@hotmail.com>
2024-02-05 15:35:51 -0300

    projeto em React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css"
+import "./App.css"
+import Contador from "./components/Contador/Contador.jsx"
+
+
+function App() {
+  return(
+    <>
+    <h1>Olá</h1>
+    <Contador />
+    </>
+  )
+}
+
+export default App
