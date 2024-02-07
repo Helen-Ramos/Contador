9d34b0e7a332c0a1f529aa1ad78b8b5c95ac3a64
Helen-Ramos <helensramos@hotmail.com>
2024-02-07 10:36:32 -0300

    complemento do contador

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,17 +1,39 @@
 import React, {useState} from "react";
+import "./Contador.css"
 
 function Contador(){
 
 const [numero, setNumero] = useState(0)
 
-return(
-        <>
-        <h1> {numero} </h1>
-        <button> + </button>
-        <button> - </button>
-        <button> C </button>
+//Função para adicionar um valor quando eu clicar no botão +
 
-        </>
+function Adicionar (){
+    if (numero < 20){
+    setNumero(numero + 1) // setNumero atualiza / modifica / seta o numero: set Numero ( o que quero que ele faça) pega o numero e soma 1 valor: setNumero(numero +1)
+    }
+}
+
+//Função para diminuir um valor quando eu clicar no botão -
+
+function Remover (){
+    if(numero > 0){
+    setNumero(numero - 1)
+    }
+}
+
+//Função para limpar o valor (voltar ao estado inicial), quando eu clicar no botão C
+
+function Limpar (){
+    setNumero (0)
+}
+
+return(
+        <div className="boxContador">
+        <h1> {numero} </h1>
+        <button onClick={Adicionar}>  + </button>
+        <button onClick={Remover}> - </button>
+        <button onClick={Limpar}> C </button>
+        </div>
     )
 }
 
